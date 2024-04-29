7964d8c4a31fa3515e1554c2d4d1bbf49aedbd79
ChenMokeev <chint2184@gmail.com>
2024-04-30 03:41:12 +0600

    fonts and shadows

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,1 +1,10 @@
-abstract class AppColors {}
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundColor = Color(0xffE1E9FC);
+  static const shadowColor = Color(0xffBABEEA);
+  static const textColor = Color(0xff4E299E);
+  static const textColor2 = Color(0xff5E3CA7);
+  static const textColor3 = Color(0xffA991DB);
+  static const greyTextColor = Color(0xff666666);
+}
--- lib/core/theme/app_shadows.dart
+++ lib/core/theme/app_shadows.dart
@@ -1,1 +1,22 @@
-abstract class AppShadows {}
+import 'package:flutter/material.dart';
+import 'package:insight_box/core/theme/app_colors.dart';
+
+abstract class AppShadows {
+  static const defaultShadow = [
+    BoxShadow(
+      blurRadius: 16,
+      offset: Offset(7, 6),
+      color: AppColors.shadowColor,
+    ),
+    BoxShadow(
+      blurRadius: 16,
+      offset: Offset(-7, -6),
+      color: Colors.white,
+    ),
+    BoxShadow(
+      blurRadius: 16,
+      offset: Offset(-4, -4),
+      color: Color(0x44ffffff),
+    ),
+  ];
+}
